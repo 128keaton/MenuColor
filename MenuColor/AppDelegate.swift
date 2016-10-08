05acc76322caad962e465dca05479836726aa350
Keaton Burleson <keaton.burleson@me.com>
2016-10-08 11:43:02 -0500

    0.1 - I have no idea what I'm doing

--- MenuColor/AppDelegate.swift
+++ MenuColor/AppDelegate.swift
@@ -11,11 +11,153 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-	@IBOutlet weak var window: NSWindow!
+	@IBOutlet var statusMenu: NSMenu!
+	let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+	@IBAction func didClickQuit(_ sender: AnyObject) {
+		NSApplication.shared().terminate(self)
+	}
 
-
+	var active = false
+	var currentColor: NSColor?
+	var currentImage: NSImage?
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
+
+
+		statusItem.menu = statusMenu
+		statusItem.image = self.roundCorners(image: NSImage.swatchWithColor(color: NSColor.white, size: NSSize(width: 10, height: 10)), width: 10, height: 10)
+		
+			NSEvent.addGlobalMonitorForEvents(matching: NSEventMask.mouseMoved) { ( event) in
+				if self.active == true {
+					guard let image = self.getColor() else {
+						print("Whoops, no color!")
+						return
+					}
+					
+					
+					
+					self.statusItem.view?.layer?.borderColor = NSColor.white.cgColor
+					self.statusItem.view?.layer?.cornerRadius = 8
+					
+					let actualImage = NSImage(cgImage: image, size: NSSize(width: 10, height: 10))
+					
+					
+					self.statusItem.image = self.roundCorners(image: actualImage, width: 10, height: 10)
+					self.currentImage = self.statusItem.image
+					let mouseLoc = NSEvent.mouseLocation()
+					let actualImage2 = NSImage(cgImage: image, size: NSSize(width: 60, height: 60))
+					let cur = NSCursor.init(image: actualImage2, hotSpot: mouseLoc)
+					cur.push()
+					cur.pop()
+				}
+			}
+			
+			 NSEvent.addGlobalMonitorForEvents(matching: NSEventMask.leftMouseDown) { (event) in
+			if self.active == true{
+				if self.currentColor != nil{
+				if self.statusMenu.items.count != 6 {
+					let item = NSMenuItem(title: "UIColor(red: \(self.currentColor!.redComponent), green: \(self.currentColor!.greenComponent), blue: \(self.currentColor!.blueComponent), alpha: \(self.currentColor!.alphaComponent)) ", action: #selector(AppDelegate.copyToClipboard(sender:)), keyEquivalent: "color")
+					item.image = self.currentImage
+					self.statusMenu.insertItem(item, at: 3)
+					
+				} else {
+					self.statusMenu.removeItem(at: 3)
+				}
+		}
+		
+				self.active = false
+			}
+		}
 		// Insert code here to initialize your application
+	}
+	func roundCorners(image: NSImage, width: CGFloat = 192, height: CGFloat = 192) -> NSImage {
+		let xRad = width / 2
+		let yRad = height / 2
+		let existing = image
+		let esize = existing.size
+		let newSize = NSMakeSize(esize.width, esize.height)
+		let composedImage = NSImage(size: newSize)
+
+		composedImage.lockFocus()
+		let ctx = NSGraphicsContext.current()
+		ctx?.imageInterpolation = NSImageInterpolation.high
+		NSColor.white.set()
+		let imageFrame = NSRect(x: 0, y: 0, width: width, height: height)
+		let clipPath = NSBezierPath(roundedRect: imageFrame, xRadius: xRad, yRadius: yRad)
+
+		clipPath.windingRule = NSWindingRule.evenOddWindingRule
+		clipPath.addClip()
+		clipPath.appendRoundedRect(NSRect(x: 0, y: 0, width: width, height: height), xRadius: xRad, yRadius: yRad)
+		clipPath.lineWidth = 2
+		clipPath.stroke()
+
+		let rect = NSRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+		image.draw(at: NSZeroPoint, from: rect, operation: NSCompositingOperation.sourceOver, fraction: 1)
+		composedImage.unlockFocus()
+
+		return composedImage
+	}
+
+	@IBAction func setActive(sender: NSMenuItem) {
+		if active == false{
+			let cur = NSCursor.crosshair()
+			cur.push()
+			cur.pop()
+			active = true
+		}
+	}
+
+	func getColor() -> CGImage? {
+		let mouseLoc = NSEvent.mouseLocation()
+
+
+
+		var displayCount: UInt32 = 0;
+		var result = CGGetActiveDisplayList(0, nil, &displayCount)
+		if (result != .success) {
+			print("error: \(result)")
+
+		}
+
+		let allocated = Int(displayCount)
+		let activeDisplays = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity: allocated)
+		result = CGGetActiveDisplayList(displayCount, activeDisplays, &displayCount)
+		if (result != .success) {
+			print("error: \(result)")
+
+		}
+		print("\(displayCount) displays:")
+		for i in 0..<displayCount {
+			print("[\(i)] - \(activeDisplays[Int(i)])")
+		}
+		activeDisplays.deallocate(capacity: allocated)
+
+		var displayID: CGDirectDisplayID = 0
+
+		let point = CGGetDisplaysWithPoint(mouseLoc, 1, &displayID, &displayCount)
+
+		if point != CGError.success {
+
+		}
+
+		guard let image = CGDisplayCreateImage(CGMainDisplayID(), rect: CGRect(x: mouseLoc.x, y: mouseLoc.y, width: 0, height: 0)) else {
+			print("woops")
+			return nil
+		}
+		let bitmap = NSBitmapImageRep.init(cgImage: image)
+
+		let color = bitmap.colorAt(x: 0, y: 0)
+		
+		currentColor = color
+		print("UIColor(red: \(color!.redComponent), green: \(color!.greenComponent), blue: \(color!.blueComponent), alpha: \(color!.alphaComponent)) ")
+
+	
+		return image
+	}
+
+	@IBAction func copyToClipboard(sender: NSMenuItem) {
+		let pasteboard = NSPasteboard.general()
+		pasteboard.clearContents()
+		pasteboard.writeObjects([sender.title as NSPasteboardWriting])
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
@@ -24,4 +166,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 }
-
+extension NSImage {
+	class func swatchWithColor(color: NSColor, size: NSSize) -> NSImage {
+		let image = NSImage(size: size)
+		image.lockFocus()
+		color.drawSwatch(in: NSMakeRect(0, 0, size.width, size.height))
+		image.unlockFocus()
+		return image
+	}
+}
